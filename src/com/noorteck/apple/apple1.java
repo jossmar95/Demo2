cfc888194b6f97d0231e1ca5f75be1f47aadee08
USUARIO <USUARIO@Jossuer>
2023-01-10 20:49:13 -0600

    Message

--- src/com/noorteck/apple/apple1.java
+++ src/com/noorteck/apple/apple1.java
@@ -1,5 +1,16 @@
 package com.noorteck.apple;
 
 public class apple1 {
-
+ 
+	public static void main(String []args) {
+		
+		System.out.println("GitHun");
+		
+	}
+	
+	
+	public static void eat() {
+		
+		
+	}
 }
